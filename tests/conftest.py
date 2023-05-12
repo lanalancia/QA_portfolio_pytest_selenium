2bf935671375afe430efde53cca866996d0af8cd
lanalancia <babahgromimolniya@gmail.com>
2023-05-12 22:57:47 +0400

    Created a global fixture container

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pytest
+
+
+@pytest.fixture
+def create_webdriver():
+    return webdriver.Chrome(service=Service(r"chromedriver.exe"))
